84e44a823a540cf2c61e0dabf0e58061763ff3c3
Jordan Pritt <pritt.jordan@gmail.com>
2020-12-01 21:24:04 -0500

    Added day1 of 2020.

---
+++ 2020/day_one/src/main.rs
@@ -0,0 +1,42 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn main() {
+    println!("Opening file...");
+    let location = Path::new("input.txt");
+    let nums = load_from_file(location);
+
+    calculate_pairs(nums);
+}
+
+fn calculate_pairs(numbers: Vec<i64>) {
+    let nums_one = &numbers;
+    let nums_two = &numbers;
+    let nums_three = &numbers;
+    'outer: for num1 in nums_one {
+        for num2 in nums_two {
+            for num3 in nums_three {
+                if num1 + num2 + num3 == 2020 {
+                    let result = num1 * num2 * num3;
+                    println!("{}", result);
+                    break 'outer;
+                }
+            }
+        }
+    }
+}
+
+fn load_from_file(file_path: &Path) -> Vec<i64> {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let numbers: Vec<i64> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<i64>().unwrap())
+        .collect();
+
+    return numbers;
+}
